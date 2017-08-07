fe0bd7d1e3ccdcb40760ffac33af7abc7d2f38a1
DESKTOP-2159NVF\Heominho <alsgh2045@naver.com>
2017-08-07 13:35:33 +0900

    no message

---
+++ test3/라인검출.cpp
@@ -0,0 +1,58 @@
+﻿/*#include <opencv2/highgui.hpp>
+#include <opencv/cv.hpp>
+using namespace cv;
+
+int main()
+{
+	IplImage* Image;
+	
+	int height, width;
+	uchar *data;
+	int i, j;
+
+	Image = cvLoadImage("siba1.jpg", 0);
+
+	height = Image->height;
+	width = Image->width;
+	data = (uchar *)Image->imageData;
+
+	printf("%8d" "%8d", height, width);
+
+	for (i = 0;i < height / 2;i++)
+	{
+		for (j = 0;j < width;j++)
+		{
+			if (data[i*width + j] > 110)
+			{
+				data[i*width + j] = 255;
+			}
+
+			else
+			{
+				data[i*width + j] = 0;
+			}
+		}
+	}
+
+	/*
+	for (i = 0;i < height;i++)
+	{
+	for (j = 0;j < width;j++)
+	{
+	printf("%8d", data[i*width + j]);
+	}
+	}
+	
+
+	cvNamedWindow("siba1", CV_WINDOW_AUTOSIZE);
+	cvShowImage("siba1", image);
+
+	cvWaitKey(0);
+	cvReleaseImage(&image);
+	
+
+	return 0;
+}
+
+*/
+
---
+++ test3/최종.cpp
@@ -0,0 +1,62 @@
+
+#include <opencv/cv.hpp>				// 헤더파일 불러오기
+#include <opencv2/highgui.hpp>
+
+
+int main()
+{
+	IplImage * srcImage;
+
+	uchar *data;
+	int i, j;
+	int height, width, size;
+
+	srcImage = cvLoadImage("siba.jpg", 0);
+
+	height = srcImage->height;
+	width = srcImage->width;
+	data = (uchar *)srcImage->imageData;
+
+	printf("기존픽셀 : height = %3d width = %3d \n", height, width);
+
+	IplImage * amendedImage = cvCreateImage(cvSize(300, 200), IPL_DEPTH_8U, 1);
+
+	cvResize(srcImage, amendedImage, CV_INTER_LINEAR); // 입력영상 srcImage를 128X128 크기의 출력영상 dstImage128로 CV_INTER_LINEAR 보간하여 크기를 변경
+
+	height = amendedImage->height;
+	width = amendedImage->width;
+	data = (uchar *)amendedImage->imageData;
+
+	printf("수정픽셀 : height = %3d width = %3d\n", height, width);
+	scanf("%d", &size);
+
+	cvNamedWindow("기존사진", CV_WINDOW_AUTOSIZE);
+	cvShowImage("기존사진", srcImage);
+
+	for (i = 0;i < height;i++)
+	{
+		for (j = 0;j < width;j++)
+		{
+			if (data[i*width + j] > 110)
+			{
+				data[i*width + j] = 255;
+			}
+
+			else
+			{
+				data[i*width + j] = 0;
+			}
+		}
+	}
+
+	cvNamedWindow("수정사진", CV_WINDOW_AUTOSIZE);
+	cvShowImage("수정사진", amendedImage);
+
+	cvWaitKey(0);
+	cvDestroyAllWindows();
+
+	cvReleaseImage(&srcImage);
+	cvReleaseImage(&amendedImage);
+
+	return 0;
+}
