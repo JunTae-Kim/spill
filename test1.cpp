1ef080168a0265ab02b4dabed09111568c97800d
JunTae-Kim <zzk1515@naver.com>
2017-08-18 10:22:09 +0900

    update -jun

--- test1.cpp
+++ test1.cpp
@@ -17,39 +17,30 @@ using namespace std;
 
 int main()
 {
-	int width = 320;
-	int height = 240;
+	int width = 640;
+	int height = 480;
 
 	raspicam::RaspiCam_Cv cam;
 
 	cam.set(CV_CAP_PROP_FORMAT, CV_8UC3);
-	cam.set(CV_CAP_PROP_FRAME_WIDTH, 320);
-	cam.set(CV_CAP_PROP_FRAME_HEIGHT, 240);
+	cam.set(CV_CAP_PROP_FRAME_WIDTH, width);
+	cam.set(CV_CAP_PROP_FRAME_HEIGHT, height);
 
 	if (!cam.open()) {
 		cerr << "Camera open failed!" << endl;
 		return -1;
 	}
-/*
-	CascadeClassifier cascade("haarcascade_frontalface_default.xml");
-
-	if (cascade.empty()) {
-		cerr << "Failed to open xml file!" << endl;
-		return -1;
-	}
-*/
 
 	if (wiringPiSetup() == -1) return 1; //wiringPi error
 
 	Mat image, edgeimg, to_hsv1, lower_red_hue_range, upper_red_hud_range, red_hue_image, hsv, ROIframe;
-	int64 t1, t2;
-	bool do_flip = true;
-	int i, j, tag;
+//	int64 t1, t2;
+	bool do_flip = false;
+	int tag;
 	int n = 3;
 	Point pt1, pt2, pt3, pt4;
-	float angle, angle2;
+//	float angle, angle2;
 	softPwmCreate(SERVO, 0, RANGE);
-
 
 	vector<Vec2f> lines;
 	vector<Mat> ROI_planes;
@@ -62,8 +53,8 @@ int main()
 		if (do_flip)
 			flip(image, image, -1);
 
-		GaussianBlur(ROIframe, ROIframe, Size(3, 3), 0, 0);
-		Canny(image, edgeimg, 550, 600);
+		GaussianBlur(ROIframe, ROIframe, Size(n, n), 0, 0);
+		Canny(image, edgeimg, 350, 400);
 
 		cvtColor(ROIframe, hsv, CV_BGR2HSV);
 		cvtColor(ROIframe, to_hsv1, CV_BGR2HSV);
@@ -75,26 +66,19 @@ int main()
 		Mat element = getStructuringElement(element_shape, Size(n,n));
 		dilate(red_hue_image, red_hue_image, element);
 
-//		Canny(image, edgeimg, 550, 600);
-
-//		HoughLines(ROIframe, lines, 1, CV_PI / 180, 0, 100);
-//		HoughLines(ROIframe, lines, 1, CV_PI / 180, 150, 0, 0);
-//		HoughLines(edgeimg, lines, 1, CV_PI / 180, 0, 100);
 		HoughLines(edgeimg, lines, 1, CV_PI / 180, 100, 0, 0);
-//		Canny(image, edgeimg, 550, 600);
 
-		t1 = getTickCount();
-//		printf("%d\n",lines.size());
+//		t1 = getTickCount();
 
 		for (size_t i = 0; i < lines.size(); i++) // °ËÃâµÈ Æ÷ÀÎÆ®žŠ Â÷Œ±Àž·Î ¿¬°á. 
 		{
 			float a1, a2;
 			float rho = lines[i][0], theta = lines[i][1];
-			float theta1, theta2, theta3;
-			float rho1, rho2, rho3;
+			float theta1, theta2;
+			float rho1, rho2;
 			int length = 800;
 			
-			if (tag == 1)
+			if (tag != 0)
 			{
 				pt1.x=0;
 				pt1.y=0;
@@ -118,8 +102,8 @@ int main()
 				pt2.x = cvRound(x0 + length * (-b));
 				pt2.y = cvRound(y0 + length * (a));
 
-				angle = (atan2(pt1.y - pt2.y, pt1.x - pt2.x))*(180 / CV_PI);
-				printf("floats : %f\n", angle); //printing angle 
+//				angle = (atan2(pt1.y - pt2.y, pt1.x - pt2.x))*(180 / CV_PI);
+//				printf("floats : %f\n", angle); //printing angle 
 
 				tag = 0;
 			}
@@ -136,8 +120,8 @@ int main()
 				pt4.x = cvRound(x02 + length * (-b2));
 				pt4.y = cvRound(y02 + length * (a2));
 
-				angle2 = (atan2(pt3.y - pt4.y, pt3.x - pt4.x))*(180 / CV_PI);
-				printf("floats2 : %f\n", angle2); //printing angle °¢µµÈ®ÀÎÀ» À§ÇÑ ÇÁž°Æ®ÄÚµå 
+//				angle2 = (atan2(pt3.y - pt4.y, pt3.x - pt4.x))*(180 / CV_PI);
+//				printf("floats2 : %f\n", angle2); //printing angle °¢µµÈ®ÀÎÀ» À§ÇÑ ÇÁž°Æ®ÄÚµå 
 
 				tag = 0;
 			}
@@ -146,41 +130,56 @@ int main()
 
 		if (waitKey(30) == 27) {
 			cout << "esc key is pressed by user" << endl;
+			break;
 		}
 
-		else if (pt1.x != 0 && pt3.x != 0) { //forward 
+		else if (pt1.x != 0 && pt3.x != 0) {
 			line(image, pt1, pt2, Scalar(255, 0, 0), 2, CV_AA);
 			line(image, pt3, pt4, Scalar(0, 0, 255), 2, CV_AA);
 
-			printf("forward\n");
+			if (tag != 1)
+			{
+				printf("forward\n");
+			}
 			softPwmWrite(SERVO, 15);
-			delay(300);
+			delay(500);
 			tag = 1;
 
 		}
 
-		else if (pt1.x != 0 && pt3.x == 0) { //left 
+		else if (pt1.x != 0 && pt3.x == 0) {
 			line(image, pt1, pt2, Scalar(255, 0, 0), 2, CV_AA);
 
-			printf("right\n");
+			if (tag != 2)
+			{
+				printf("right\n");
+			}
 			softPwmWrite(SERVO, 18);
-			delay(300);
-			tag = 1;
+			delay(500);
+			tag = 2;
 		}
 
-		else if (pt1.x == 0 && pt3.x != 0) { //right 
+		else if (pt1.x == 0 && pt3.x != 0) {
 			line(image, pt3, pt4, Scalar(0, 0, 255), 2, CV_AA);
 
-			printf("left\n");
+			if (tag != 3)
+			{
+				printf("left\n");
+			}
 			softPwmWrite(SERVO, 12);
-			delay(300);
-			tag = 1;
+			delay(500);
+			tag = 3;
 		}
 
 		else {
+			if (tag != 4)
+			{
+				printf("No detect Line\n");
+			}
+			tag = 4;
 		}
 
-		t2 = getTickCount();
+//		t2 = getTickCount();
 //		cout << "It took " << (t2 - t1) * 1000 / getTickFrequency() << " ms." << endl;
 
 		imshow("Camera1", image);
@@ -193,122 +192,6 @@ int main()
 			do_flip = !do_flip;
 
 	} // While End 
-
-
-
-/*
-		unsigned char* data = (unsigned char*)img.data;
-
-		t1 = getTickCount();
-
-
-		for (i = 0;i < height;i++)
-		{
-			for (j = 0;j < width;j++)
-			{
-				if (data[i*width + j] > 110)
-				{
-					data[i*width + j] = 255;
-
-					if (j==319 && i>160)
-					{
-						data[i*width + j] = 0;
-					}
-				}
-	
-				else
-				{
-					data[i*width + j] = 0;
-					
-					if (j==319 && i>160)
-					{
-						data[i*width + j] = 255;
-					}
-				}
-			}
-		}
-
-		img.data=data;
-		Canny(img, edgeimg, 600, 750);
-
-		for (i=0;i<width;i++)
-		{
-			if (data[200*width + i] == 255)
-			{
-				break;
-			}
-		}
-
-		for (j=width;j>0;j--)
-		{
-			if (data[200*width + j] == 255)
-			{
-				break;
-			}
-		}
-
-		middot1 = (int)((i+j) / 2);
-
-		for (i=0;i<width;i++)
-		{
-			if (data[300*width + i] == 255)
-			{
-				break;
-			}
-		}
-
-		for (j=width;j>0;j--)
-		{
-			if (data[300*width + j] == 255)
-			{
-				break;
-			}
-		}
-
-		middot2 = (int)((i+j) / 2);
-
-		dir = middot2 - middot1; 
-
-		if (abs(dir) < 30)
-		{
-			printf("go on\n");
-		}
-		else if (30<=dir && dir<=50)
-		{
-			printf("left turn\n");
-		}
-		else if (dir>50)
-		{
-			printf("strong left turn\n");
-		}
-		else if (-50<=dir && dir<=-30)
-		{
-			printf("right turn\n");
-		}
-		else if (dir<-50)
-		{
-			printf("strong right turn\n");
-		}
-		else
-		{
-			printf("error\n");
-		}
-
-		t2 = getTickCount();
-		cout << "It took " << (t2 - t1) * 1000 / getTickFrequency() << " ms." << endl;
-
-		imshow("cam", img);
-		imshow("cam2", edgeimg);
-
-
-		int k = waitKey(1);
-		if (k == 27)
-			break;
-		else if (k == 'f' || k == 'F')
-			do_flip = !do_flip;
-	}
-
-*/
 
 	cam.release();
 	destroyAllWindows();
